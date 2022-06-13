43232e0fb306877ad762a8f34318844612c91ade
Nikita <nikitax160@yandex.ru>
2022-06-13 20:30:53 +0300

    Start

---
+++ src/test/java/com/nikitax/simpleREST/SimpleRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikitax.simpleREST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
